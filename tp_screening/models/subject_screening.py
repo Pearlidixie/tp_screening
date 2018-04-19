b894c907368434fb2c2b56ff6f79bdec139a8f4b
pmalapeng <pmalapeng@gmail.com>
2018-04-19 09:13:34 +0200

    created model SubjectScreening with its fields

--- tp_screening/models/subject_screening.py
+++ tp_screening/models/subject_screening.py
@@ -2,35 +2,45 @@ from django.db import models
 from edc_constants.choices import GENDER, YES_NO, YES_NO_NA
 
 
-class EnrollmentChecklist():
-
+class SubjectScreening(models.Model):
+    print("models")
     gender = models.CharField(
         max_length=10,
         choices=GENDER)
 
     is_citizen = models.CharField(
         verbose_name="Are you a Botswana citizen?",
+        max_length=10,
         choices=YES_NO)
 
     is_married_citizen = models.CharField(
         verbose_name="If not a citizen, are you legally married to a Botswana citizen?",
+        max_length=10,
         choices=YES_NO_NA)
 
     marriage_proof = models.CharField(
         verbose_name="Has the participant produced the marriage certificate, as proof?",
+        max_length=10,
         choices=YES_NO_NA)
 
     is_literate = models.CharField(
         verbose_name="Is the participant literate?",
+        max_length=10,
         choices=YES_NO)
 
     literate_witness_avail = models.CharField(
         verbose_name="If illeterate, is there a literate witness available?",
+        max_length=10,
         choices=YES_NO_NA)
 
     is_minor = models.CharField(
+        max_length=10,
         choices=YES_NO)
 
     guardian_available = models.CharField(
         verbose_name="If minor, is there a guardian available?",
+        max_length=10,
         choices=YES_NO_NA)
+
+    def __str__(self):
+        return self.name
