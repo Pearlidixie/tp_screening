196d9f20704864906e7aba426147b90392976d9d
pmalapeng <pmalapeng@gmail.com>
2018-04-20 15:57:17 +0200

    made the subject screening form to inherit from FormValidatorMixin

--- tp_screening/forms/subject_screening_form.py
+++ tp_screening/forms/subject_screening_form.py
@@ -1,8 +1,13 @@
 from ..models import SubjectScreening
 from django import forms
+from tp_subject_form_validators.form_validators.subject_screening import SubjectScreeningFormValidator
+from edc_form_validators import FormValidatorMixin
 
 
-class SubjectScreeningForm(forms.ModelForm):
+class SubjectScreeningForm(FormValidatorMixin, forms.ModelForm):
+
+    form_validator_cls = SubjectScreeningFormValidator
+
     class Meta:
         model = SubjectScreening
         fields = '__all__'
