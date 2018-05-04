e4c89b76db60894a619c6568fef44b390238f475
pmalapeng <pmalapeng@gmail.com>
2018-05-04 08:27:02 +0200

    removed the is_minor field, changed the order in which the fields are displayed

--- tp_screening/admin/subject_screening_admin.py
+++ tp_screening/admin/subject_screening_admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from tp_screening.models.subject_screening import SubjectScreening
+from tp_screening.forms.subject_screening_form import SubjectScreeningForm
 #from tp_screening.admin_site import tp_screening_admin
 
 
 #@admin.register(SubjectScreening, site=tp_screening_admin)
 class SubjectScreeningAdmin(admin.ModelAdmin):
+    form = SubjectScreeningForm
 
     radio_fields = {
         'gender': admin.VERTICAL,
@@ -13,21 +15,22 @@ class SubjectScreeningAdmin(admin.ModelAdmin):
         'marriage_proof': admin.VERTICAL,
         'is_literate': admin.VERTICAL,
         'literate_witness_avail': admin.VERTICAL,
-        'is_minor': admin.VERTICAL,
-        'guardian_available': admin.VERTICAL}
+        'guardian_available': admin.VERTICAL,
+        'consent_ability': admin.VERTICAL}
 
     fieldsets = (
         (None, {
             'fields': (
                 'report_datetime',
                 'gender',
+                'age_in_years',
+                'guardian_available',
                 'is_citizen',
                 'is_married_citizen',
                 'marriage_proof',
                 'is_literate',
                 'literate_witness_avail',
-                'is_minor',
-                'guardian_available',)
+                'consent_ability')
         }),
 
     )
